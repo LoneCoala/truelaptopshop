90ae5ed562dbb3e8cc7a10c7d0bdaf62c92eae83
Даниил Тюрин <tjurin.daniil@rambler.ru>
2023-06-14 17:07:19 +0700

    second commit working api

--- app/src/main/java/com/example/truelaptopshop/MainActivity.kt
+++ app/src/main/java/com/example/truelaptopshop/MainActivity.kt
@@ -3,26 +3,53 @@ package com.example.truelaptopshop
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import okhttp3.Callback
 import okhttp3.OkHttpClient
+import retrofit2.Call
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
-    var client: OkHttpClient = OkHttpClient()
-    var getURL: String = ""
-    var postURL: String = ""
-    var TextView: TextView? = null
-    var buttonGet: Button? = null
-    var buttonPost: Button? = null
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        /*
         TextView = findViewById(R.id.TextData)
         buttonGet = findViewById(R.id.btnGet)
-        buttonPost = findViewById(R.id.btnPost)
         buttonGet!!.setOnClickListener{TextView!!.text = "dasdasd"
+*/
+
+
+        val ServiceGenerator = ServiceGenerator.buildService(ApiInterface::class.java)
+        val call = ServiceGenerator.getPosts()
+
+        val btnGet = findViewById<Button>(R.id.btnGet)
+        btnGet.setOnClickListener {
+            call.enqueue(object: retrofit2.Callback<MutableList<PostModel>>{
+                override fun onResponse(
+                    call: Call<MutableList<PostModel>>,
+                    response: Response<MutableList<PostModel>>
+                ) {
+                    if (response.isSuccessful()){
+                        Log.e("sucess", response.body().toString())
+                    }
+                }
+
+
+                override fun onFailure(call: Call<MutableList<PostModel>>, t: Throwable) {
+                    t.printStackTrace()
+                    Log.e("error", t.message.toString())
+
+                }
+
+            })
         }
     }
+}
+
+private fun <T> Call<T>.enqueue(any: Any) {
+
 }
---
+++ app/src/main/java/com/example/truelaptopshop/PostModel.kt
@@ -0,0 +1,15 @@
+package com.example.truelaptopshop
+
+data class PostModel (
+    val id: Int? = null,
+    val name: String? = null,
+    val gpu: String? = null,
+    val cpu: String? = null,
+    val ram: Int? = null,
+    val ssd: Int? = null,
+    val screen: Int? = null,
+    val os: String? = null,
+    val color: String? = null,
+    val pic: String? = null,
+    val price: Int? = null
+)

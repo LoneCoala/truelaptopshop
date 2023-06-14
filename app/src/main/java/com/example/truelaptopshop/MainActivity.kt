d86bad83beab2b2486e24ca3013c8fa7d8c91498
Даниил Тюрин <tjurin.daniil@rambler.ru>
2023-06-14 21:07:56 +0700

    third commit RecycleView + photo from Picasso

--- app/src/main/java/com/example/truelaptopshop/MainActivity.kt
+++ app/src/main/java/com/example/truelaptopshop/MainActivity.kt
@@ -5,7 +5,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.imageview.ShapeableImageView
+import com.squareup.picasso.Picasso
 import okhttp3.Callback
 import okhttp3.OkHttpClient
 import retrofit2.Call
@@ -15,7 +21,14 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
         setContentView(R.layout.activity_main)
+
+
+/*
+        setContentView(R.layout.list_item)
+*/
         /*
         TextView = findViewById(R.id.TextData)
         buttonGet = findViewById(R.id.btnGet)
@@ -25,28 +38,33 @@ class MainActivity : AppCompatActivity() {
 
         val ServiceGenerator = ServiceGenerator.buildService(ApiInterface::class.java)
         val call = ServiceGenerator.getPosts()
+        val recyclerView = findViewById<RecyclerView>(R.id.recycleView)
 
-        val btnGet = findViewById<Button>(R.id.btnGet)
-        btnGet.setOnClickListener {
-            call.enqueue(object: retrofit2.Callback<MutableList<PostModel>>{
-                override fun onResponse(
-                    call: Call<MutableList<PostModel>>,
-                    response: Response<MutableList<PostModel>>
-                ) {
-                    if (response.isSuccessful()){
-                        Log.e("sucess", response.body().toString())
+
+        call.enqueue(object: retrofit2.Callback<MutableList<PostModel>>{
+            override fun onResponse(
+                call: Call<MutableList<PostModel>>,
+                response: Response<MutableList<PostModel>>
+            ) {
+                if (response.isSuccessful()){
+
+                    recyclerView.apply {
+                        layoutManager = LinearLayoutManager(this@MainActivity)
+                        setHasFixedSize(true)
+                        adapter = PostAdapter(response.body()!!)
                     }
                 }
+            }
+
+            override fun onFailure(call: Call<MutableList<PostModel>>, t: Throwable) {
+                t.printStackTrace()
+                Log.e("error", t.message.toString())
+
+            }
+
+        })
 
 
-                override fun onFailure(call: Call<MutableList<PostModel>>, t: Throwable) {
-                    t.printStackTrace()
-                    Log.e("error", t.message.toString())
-
-                }
-
-            })
-        }
     }
 }
 
